d295ec9c2d0041958a591e963acfdb6df290c0e2
Jarek <jarek.kon0709@gmail.com>
2026-03-26 13:44:39 +0000

    Update frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,26 +1,25 @@
-import React, { Suspense, lazy } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import SmoothScrollProvider from "./components/SmoothScrollProvider";
 import ScrollToTop from "./components/ScrollToTop";
 
-// Lazy-loaded routes
-const Home = lazy(() => import("./pages/Home"));
-const AboutMe = lazy(() => import("./pages/AboutMe"));
+import Home from "./pages/Home";
+import AboutMe from "./pages/AboutMe";
 
-const OurProjects = lazy(() => import("./pages/OurProjects"));
-const Project1 = lazy(() => import("./pages/Project1"));
-const Project2 = lazy(() => import("./pages/Project2"));
+import OurProjects from "./pages/OurProjects";
+import Project1 from "./pages/Project1";
+import Project2 from "./pages/Project2";
 
-const CustomService = lazy(() => import("./pages/CustomService"));
-const EcommerceService = lazy(() => import("./pages/EcommerceService"));
+import CustomService from "./pages/CustomService";
+import EcommerceService from "./pages/EcommerceService";
 
-const Blog = lazy(() => import("./pages/Blog"));
-const BlogPage1 = lazy(() => import("./pages/BlogPage1"));
-const BlogPage2 = lazy(() => import("./pages/BlogPage2"));
-const BlogPage3 = lazy(() => import("./pages/BlogPage3"));
+import Blog from "./pages/Blog";
+import BlogPage1 from "./pages/BlogPage1";
+import BlogPage2 from "./pages/BlogPage2";
+import BlogPage3 from "./pages/BlogPage3";
 
-const BookACall = lazy(() => import("./pages/BookACall"));
+import BookACall from "./pages/BookACall";
 
 function App() {
   return (
@@ -28,48 +27,40 @@ function App() {
       <ScrollToTop />
 
       <SmoothScrollProvider>
-        <Suspense fallback={null}>
-          <Routes>
-            {/* Home */}
-            <Route path="/" element={<Home />} />
+        <Routes>
+          <Route path="/" element={<Home />} />
 
-            {/* About */}
-            <Route path="/about" element={<AboutMe />} />
+          <Route path="/about" element={<AboutMe />} />
 
-            {/* Projects overview + case studies */}
-            <Route path="/projects" element={<OurProjects />} />
-            <Route path="/projects/konarcard" element={<Project1 />} />
-            <Route path="/projects/azzurra" element={<Project2 />} />
+          <Route path="/projects" element={<OurProjects />} />
+          <Route path="/projects/konarcard" element={<Project1 />} />
+          <Route path="/projects/azzurra" element={<Project2 />} />
 
-            {/* Services */}
-            <Route
-              path="/services/custom"
-              element={<CustomService />}
-            />
-            <Route
-              path="/services/ecommerce"
-              element={<EcommerceService />}
-            />
+          <Route
+            path="/services/custom"
+            element={<CustomService />}
+          />
+          <Route
+            path="/services/ecommerce"
+            element={<EcommerceService />}
+          />
 
-            {/* Blog */}
-            <Route path="/blog" element={<Blog />} />
-            <Route
-              path="/blog/growth-insights"
-              element={<BlogPage1 />}
-            />
-            <Route
-              path="/blog/conversion-playbooks"
-              element={<BlogPage2 />}
-            />
-            <Route
-              path="/blog/technical-foundations"
-              element={<BlogPage3 />}
-            />
+          <Route path="/blog" element={<Blog />} />
+          <Route
+            path="/blog/growth-insights"
+            element={<BlogPage1 />}
+          />
+          <Route
+            path="/blog/conversion-playbooks"
+            element={<BlogPage2 />}
+          />
+          <Route
+            path="/blog/technical-foundations"
+            element={<BlogPage3 />}
+          />
 
-            {/* Book a Call */}
-            <Route path="/book-a-call" element={<BookACall />} />
-          </Routes>
-        </Suspense>
+          <Route path="/book-a-call" element={<BookACall />} />
+        </Routes>
       </SmoothScrollProvider>
     </Router>
   );
